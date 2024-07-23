1924c91b9db8c08af143711bbc1151c5ddc9cd73
gopinathgbg <gopinath.vbg@gmail.com>
2024-07-23 23:54:46 +0530

    eureka discovery server created

---
+++ src/test/java/com/hmis/myeurekaserver/HmisDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmis.myeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmisDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
